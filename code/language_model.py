17456b9cb08b2c9a582096b6c35cc94ed8b5d7d4
assaflehr <assaflehr@gmail.com>
2018-07-02 18:38:17 +0300

    reformat

--- code/language_model.py
+++ code/language_model.py
@@ -1,47 +1,37 @@
 import os
-import sys
 import time
-#import ipdb
-import random
-import numpy as np
-import tensorflow as tf
 
-from vocab import Vocabulary, build_vocab
-from options import load_arguments
 from file_io import load_sent
-from utils import *
 from nn import *
+from options import load_arguments
+from utils import *
+from vocab import Vocabulary, build_vocab
+
+
+# import ipdb
 
 class Model(object):
-
     def __init__(self, args, vocab):
         dim_emb = args.dim_emb
         dim_z = args.dim_z
         n_layers = args.n_layers
 
-        self.dropout = tf.placeholder(tf.float32,
-            name='dropout')
-        self.learning_rate = tf.placeholder(tf.float32,
-            name='learning_rate')
-        self.batch_size = tf.placeholder(tf.int32,
-            name='batch_size')
-        self.inputs = tf.placeholder(tf.int32, [None, None],    #batch_size * max_len
-            name='inputs')
-        self.targets = tf.placeholder(tf.int32, [None, None],
-            name='targets')
-        self.weights = tf.placeholder(tf.float32, [None, None],
-            name='weights')
+        self.dropout = tf.placeholder(tf.float32, name='dropout')
+        self.learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+        self.batch_size = tf.placeholder(tf.int32, name='batch_size')
+        self.inputs = tf.placeholder(tf.int32, [None, None],  # batch_size * max_len
+                                     name='inputs')
+        self.targets = tf.placeholder(tf.int32, [None, None], name='targets')
+        self.weights = tf.placeholder(tf.float32, [None, None], name='weights')
 
-        embedding = tf.get_variable('embedding',
-            initializer=vocab.embedding.astype(np.float32))
+        embedding = tf.get_variable('embedding', initializer=vocab.embedding.astype(np.float32))
         with tf.variable_scope('projection'):
             proj_W = tf.get_variable('W', [dim_z, vocab.size])
             proj_b = tf.get_variable('b', [vocab.size])
 
         inputs = tf.nn.embedding_lookup(embedding, self.inputs)
         cell = create_cell(dim_z, n_layers, self.dropout)
-        outputs, _ = tf.nn.dynamic_rnn(cell, inputs,
-            dtype=tf.float32, scope='language_model')
+        outputs, _ = tf.nn.dynamic_rnn(cell, inputs,                                       dtype=tf.float32, scope='language_model')
         outputs = tf.nn.dropout(outputs, self.dropout)
         outputs = tf.reshape(outputs, [-1, dim_z])
         self.logits = tf.matmul(outputs, proj_W) + proj_b
@@ -58,15 +48,17 @@ class Model(object):
 
         self.saver = tf.train.Saver()
 
+
 def create_model(sess, args, vocab):
     model = Model(args, vocab)
     if args.load_model:
-        print ('Loading model from', args.model)
+        print('Loading model from', args.model)
         model.saver.restore(sess, args.model)
     else:
-        print ('Creating model with fresh parameters.')
+        print('Creating model with fresh parameters.')
         sess.run(tf.global_variables_initializer())
     return model
+
 
 def get_lm_batches(x, word2id, batch_size):
     pad = word2id['<pad>']
@@ -89,15 +81,16 @@ def get_lm_batches(x, word2id, batch_size):
             padding = [pad] * (max_len - l)
             go_x.append([go] + sent_id + padding)
             x_eos.append(sent_id + [eos] + padding)
-            weights.append([1.0] * l + [0.0] * (max_len-l+1))
+            weights.append([1.0] * l + [0.0] * (max_len - l + 1))
 
         batches.append({'inputs': go_x,
                         'targets': x_eos,
                         'weights': weights,
-                        'size': t-s})
+                        'size': t - s})
         s = t
 
     return batches
+
 
 def evaluate(sess, args, vocab, model, x):
     batches = get_lm_batches(x, vocab.word2id, args.batch_size)
@@ -105,20 +98,19 @@ def evaluate(sess, args, vocab, model, x):
 
     for batch in batches:
         tot_loss += sess.run(model.tot_loss,
-            feed_dict={model.batch_size: batch['size'],
-                       model.inputs: batch['inputs'],
-                       model.targets: batch['targets'],
-                       model.weights: batch['weights'],
-                       model.dropout: 1})
+                             feed_dict={model.batch_size: batch['size'],
+                                        model.inputs: batch['inputs'],
+                                        model.targets: batch['targets'],
+                                        model.weights: batch['weights'],
+                                        model.dropout: 1})
         n_words += np.sum(batch['weights'])
 
     return np.exp(tot_loss / n_words)
 
+
 if __name__ == '__main__':
-    #example run: --train ../data/yelp/sentiment.train.0 --dev ../data/yelp/sentiment.dev.0 --output ../tmp/sentiment.dev.0 --vocab ../tmp/yelp.vocab --model ../tmp/model
+    # example run: --train ../data/yelp/sentiment.train.0 --dev ../data/yelp/sentiment.dev.0 --output ../tmp/sentiment.dev.0 --vocab ../tmp/yelp.vocab --model ../tmp/model
     args = load_arguments()
-
-
 
     if args.train:
         train = load_sent(args.train)
@@ -127,7 +119,7 @@ if __name__ == '__main__':
             build_vocab(train, args.vocab)
 
     vocab = Vocabulary(args.vocab, args.embedding, args.dim_emb)
-    print ('vocabulary size', vocab.size)
+    print('vocabulary size', vocab.size)
 
     if args.dev:
         dev = load_sent(args.dev)
@@ -136,7 +128,7 @@ if __name__ == '__main__':
         test = load_sent(args.test)
 
     config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True  #allocate only as much GPU memory based on runtime allocations: it starts out allocating very little memory
+    config.gpu_options.allow_growth = True  # allocate only as much GPU memory based on runtime allocations: it starts out allocating very little memory
     with tf.Session(config=config) as sess:
         model = create_model(sess, args, vocab)
         if args.train:
@@ -150,36 +142,36 @@ if __name__ == '__main__':
             learning_rate = args.learning_rate
 
             for epoch in range(args.max_epochs):
-                print ('----------------------------------------------------')
-                print ('epoch %d, learning_rate %f' % (epoch + 1, learning_rate))
+                print('----------------------------------------------------')
+                print('epoch %d, learning_rate %f' % (epoch + 1, learning_rate))
 
-                for i,batch in enumerate(batches):
-                    if i% 50 ==0: print ('batch',i,batch['size'])
+                for i, batch in enumerate(batches):
+                    if i % 50 == 0: print('batch', i, batch['size'])
                     step_loss, _ = sess.run([model.sent_loss, model.optimizer],
-                        feed_dict={model.batch_size: batch['size'],
-                                   model.inputs: batch['inputs'],
-                                   model.targets: batch['targets'],
-                                   model.weights: batch['weights'],
-                                   model.dropout: args.dropout_keep_prob,
-                                   model.learning_rate: learning_rate})
+                                            feed_dict={model.batch_size: batch['size'],
+                                                       model.inputs: batch['inputs'],
+                                                       model.targets: batch['targets'],
+                                                       model.weights: batch['weights'],
+                                                       model.dropout: args.dropout_keep_prob,
+                                                       model.learning_rate: learning_rate})
 
                     step += 1
                     loss += step_loss / args.steps_per_checkpoint
 
                     if step % args.steps_per_checkpoint == 0:
-                        print ('step %d, time %.0fs, loss %.2f' \
-                            % (step, time.time() - start_time, loss))
+                        print('step %d, time %.0fs, loss %.2f' \
+                              % (step, time.time() - start_time, loss))
                         loss = 0.0
 
                 if args.dev:
                     ppl = evaluate(sess, args, vocab, model, dev)
-                    print ('dev perplexity %.2f' % ppl)
+                    print('dev perplexity %.2f' % ppl)
                     if ppl < best_dev:
                         best_dev = ppl
                         unchanged = 0
-                        print ('Saving model...')
+                        print('Saving model...')
                         model.saver.save(sess, args.model)
 
         if args.test:
             ppl = evaluate(sess, args, vocab, model, test)
-            print ('test perplexity %.2f' % ppl)
+            print('test perplexity %.2f' % ppl)
